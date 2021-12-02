6e89f6608362b3ec71737ebb0c7fc236feeb42a9
mondayrain <lfeng@wealthsimple.com>
2021-12-01 23:25:52 -0800

    Day 1, I spent so much time trying to do it functionally but didn't even think to use Zip :(

---
+++ 1/solution.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def data():
+    file = open('input.txt')
+    return list(map(lambda str: int(str.strip("\n")), file.readlines()))
+
+def run():
+    increases = 0
+    lines = data()
+
+    for i in range(len(lines) - 1):
+        increases += 1 if lines[i] < lines[i+1] else 0
+
+    print(increases)
+
+run()
